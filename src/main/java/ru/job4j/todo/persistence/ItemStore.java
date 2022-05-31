c6aabfff70ce4edda3abfd88d61ca410816cf8f6
VasilievAnatoliy <vasiljev.4natoli@yandex.ru>
2022-05-31 15:41:19 +0300

    Лямбды и шаблон wrapper

--- src/main/java/ru/job4j/todo/persistence/ItemStore.java
+++ src/main/java/ru/job4j/todo/persistence/ItemStore.java
@@ -2,10 +2,12 @@ package ru.job4j.todo.persistence;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.Item;
 
 import java.util.List;
+import java.util.function.Function;
 
 @Repository
 public class ItemStore {
@@ -15,75 +17,70 @@ public class ItemStore {
         this.sf = sf;
     }
 
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
+    }
+
     public Item add(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+       tx(session -> session.save(item));
+       return item;
     }
 
     public Item findById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item item = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+       return tx(session -> session.get(Item.class, id));
     }
 
     public List findAll() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return tx(session -> session.createQuery(
+                "from Item").list()
+        );
     }
 
     public List findAllDone() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Item where done = true ").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return tx(session -> session.createQuery(
+                "from Item where done = true").list()
+        );
     }
 
     public List findAllNotDone() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List result = session.createQuery("from Item where done = false ").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return tx(session -> session.createQuery(
+                "from Item where done = false").list()
+        );
     }
 
     public void update(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
+        tx(session -> {
+            session.update(item);
+                    return null;
+                }
+        );
     }
 
     public void delete(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item item = new Item();
-        item.setId(id);
-        session.delete(item);
-        session.getTransaction().commit();
-        session.close();
+        tx(session -> {
+            Item item = new Item();
+            item.setId(id);
+            session.delete(item);
+                    return null;
+                }
+        );
     }
 
     public void done(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.createQuery("update Item s set s.done = true where s.id = :fId")
-        .setParameter("fId", id).executeUpdate();
-        session.getTransaction().commit();
-        session.close();
+        tx(session -> session.createQuery(
+                "update Item s set s.done = true where s.id = :fId")
+                .setParameter("fId", id).executeUpdate()
+        );
     }
 }
